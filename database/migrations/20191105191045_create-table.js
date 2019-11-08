6ea545b332f8a87bcadac4721e175466273d86d0
Travis Little <kingtrav@gmail.com>
2019-11-07 18:44:44 -0700

    added more profile fields to Coaches table

--- database/migrations/20191105191045_create-table.js
+++ database/migrations/20191105191045_create-table.js
@@ -7,10 +7,16 @@ exports.up = function(knex) {
         coaches.string('password', 128).notNullable()
         coaches.string('firstname', 128).notNullable()
         coaches.string('lastname', 128).notNullable()
-        coaches.integer('is_active', 1).defaultTo(1)
-        coaches.string('language', 128).notNullable()
-        coaches.string('timezone', 128).notNullable()
+        coaches.integer('is_active', 1).defaultTo(1).notNullable()
+        coaches.string('language', 128)
+        coaches.string('timezone', 128)
+        coaches.string('picture_url', 500)
+        coaches.string('city')
+        coaches.string('country')
         coaches.string('bio', 10000)
+        coaches.string('certifications', 1000) // This will not always be a string
+        coaches.string('awards')                // This will not always be a string
+        coaches.string('specialties')           // This will not always be a string
     })
 };
 
