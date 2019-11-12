ec125dedcaa4d2eaf8fc534e700b4d152b9132bf
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-11-11 19:55:45 -0600

    specialties, coach specilaty details tables created

---
+++ database/migrations/20191111194935_coach_specialty_details.js
@@ -0,0 +1,28 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('coach_specialty_details', csd => {
+      csd.increments()
+      csd
+        .integer('coach_id')
+        .notNullable()
+        .references('id')
+        .inTable('coaches')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+      csd
+        .integer('specialty_id')
+        .notNullable()
+        .references('id')
+        .inTable('specialties')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+
+      csd.timestamp()
+
+    })
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists('coach_specialty_details')
+};
