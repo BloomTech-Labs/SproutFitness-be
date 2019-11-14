987856c72f37b2584bc4b1d8739321a600bbcc57
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-11-13 19:47:02 -0600

    dbConfig file updated

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -2,7 +2,7 @@ const knex = require('knex');
 
 const config = require('../knexfile.js');
 
-const dbEnv = process.env.DB_ENV || 'development';
-// const dbEnv = process.env.NODE_ENV || 'development'; // if something else isn't setting ENV, use development
+//const environment = process.env.DB_ENV || 'development';
+const environment = process.env.NODE_ENV || 'development'; // if something else isn't setting ENV, use development
 
-module.exports = knex(config[dbEnv]);
+module.exports = knex(config[environment]);
