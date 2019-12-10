815d92b1713875a1a935c7679d3db58043a4a47c
Travis Little <kingtrav@gmail.com>
2019-12-09 20:02:23 -0700

    mirgration organization

---
+++ database/migrations/dev/01_coaches.js
@@ -0,0 +1,25 @@
+exports.up = function(knex) {
+  return knex.schema
+  .createTable('coaches', coaches => {
+      coaches.string('id', 255).notNullable().unique()
+      coaches.string('family_id').references('id').inTable('families')
+      coaches.string('email', 128).notNullable().unique()
+      coaches.string('password', 128).notNullable()
+      coaches.string('firstname', 128).notNullable()
+      coaches.string('lastname', 128).notNullable()
+      coaches.integer('is_active', 1).defaultTo(1).notNullable()
+      coaches.string('language', 128)
+      coaches.string('timezone', 128)
+      coaches.string('picture_url', 500)
+      coaches.string('city')
+      coaches.string('country')
+      coaches.string('bio', 10000)
+      coaches.string('resetPasswordToken')
+      coaches.datetime('resetPasswordExpires')
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+  .dropTableIfExists('coaches')
+};
